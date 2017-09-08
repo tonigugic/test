b76d86a2d6f7e2217c7f769dfecfb17e89066630
tonigugic <toni.gugic@gmail.com>
2017-09-08 19:56:15 +0200

    added function

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 //Event Handler
 
-window.onload = init;
 
-function init() {
+
 	var items = document.getElementsByTagName("li");
 	for (var i = 0; i < items.length; i++) {
 		items[i].onclick = bookMark;
-	}
 }
+
+console.log(items
+  );
 
 function bookMark(eventObj) {
 	var item  = eventObj.target;
+  
   var id = item.id;
+  var style = item.style.textDecoration;
+ 
 
-	document.getElementById(id).style.textDecoration = "line-through";
+  if(style == "") {
+    document.getElementById(id).style.textDecoration = "line-through";
+  } else {
+    document.getElementById(id).style.textDecoration = "";
+  }
+
+	
 		
 }	
 
